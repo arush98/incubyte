eb488f58df2f5fa569e43b755847f18a7b62ad66
Arush <220240428+arushinterspectai@users.noreply.github.com>
2025-09-17 17:10:57 +0530

    feat: handle any amount of comma-separated numbers

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,8 +1,9 @@
 const { add } = require('../src/stringCalculator');
 
 describe('String Calculator', () => {
-  test('two comma-separated numbers are summed', () => {
-  expect(add("1,5")).toBe(6);
-  expect(add("2,3")).toBe(5);
+  test('any amount of comma-separated numbers are summed', () => {
+  expect(add("1,2,3")).toBe(6);
+  expect(add("1,2,3,4,5")).toBe(15);
+  expect(add("1,2,3,4,5,6,7,8,9,10")).toBe(55);
 });
 });
